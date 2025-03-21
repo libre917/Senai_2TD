41d37b6a4a019d81f9d25a80e53d8009486362af
LUCAS SOALHEIRO PEREIRA <24250350@educ123.sp.senai.br>
2025-03-21 17:16:06 -0300

    adição em 'Front-end'

---
+++ Front_end/aula_4/app/src/Components/Header/Header.jsx
@@ -0,0 +1,14 @@
+import './header.css'
+export default function Header(){
+    return(
+        <header>
+            <nav>
+                <h2>Loja Virtual</h2>
+                <ul>
+                    <li>Home</li>
+                    <li>Sobre</li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
---
+++ Front_end/aula_4/app/src/app/page.jsx
@@ -0,0 +1,32 @@
+"use client";
+import {useState} from "react";
+
+export default function Home() {
+  const [Contador, setContator] = useState(0);
+  function handleAcrescentar(){
+    if (Contador >= 5){
+      alert('Não acrescentar mais que 5!!')
+      setContator(5)
+    } else {
+      setContator(Contador + 1)
+    }
+  }
+  function handleDecremnetar(){
+    if (Contador <= 0){
+      alert('Não decrementar mais!!')
+      setContator(0)
+    } else {
+      setContator(Contador - 1)
+    }
+  }
+  return (
+  <>
+  <h1>Contador</h1>
+  <div>
+    <button type="button" onClick={handleAcrescentar}> + </button>
+    <span>{Contador}</span>
+    <button type="buttonMenos" onClick={handleDecremnetar}> - </button>
+  </div>
+  </>
+  );
+}
